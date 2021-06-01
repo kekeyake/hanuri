4287d96e81516708f3d65f7a400f9cfb96b3d877
martin <martin@irm.kr>
2021-06-02 05:14:14 +0900

    모바일메뉴 완료

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,11 +1,5 @@
 var $winW, $winH, $isMobile;
 $(function () {
-
-    chkBrowserSize();
-    $(window).resize(function () {
-        chkBrowserSize();
-    });
-
     // ===== Scroll to Top ==== 
     $(window).scroll(function () {
         if ($(window).scrollTop() > 100) {
@@ -14,15 +8,12 @@ $(function () {
             $('header').removeClass('sticky')
         }
     });
+
+    $("#btnMobile").off().on('click',function(){
+        $('.side_nav').addClass('open');
+    });
+    $("#btnClosed").off().on('click',function(){
+        $('.side_nav').removeClass('open');
+    });
 });
 
-function chkBrowserSize() {
-    $winW = $(window).width();
-    $winH = $(window).height();
-
-    if ($winW < 1025) {
-        $isMobile = true;
-    } else {
-        $isMobile = false;
-    }
-}
